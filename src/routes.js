f107cea0bf73718065d7f8eb5ea2d62ba57714cf
Adarsh Prakash <adarshnobilian@gmail.com>
2025-01-16 17:31:59 +0530

    EmployerDetails, services, career, portfolio, projectassigned components added, employer details form ui completed

--- src/routes.js
+++ src/routes.js
@@ -38,15 +38,24 @@ Coded by www.creative-tim.com
 // Material Dashboard 2 React layouts
 import Dashboard from "layouts/dashboard";
 import Tables from "layouts/tables";
-import Billing from "layouts/billing";
-import RTL from "layouts/rtl";
-import Notifications from "layouts/notifications";
-import Profile from "layouts/profile";
+// import Billing from "layouts/billing";
+// import RTL from "layouts/rtl";
+// import Notifications from "layouts/notifications";
+// import Profile from "layouts/profile";
 import SignIn from "layouts/authentication/sign-in";
-import SignUp from "layouts/authentication/sign-up";
-
-// @mui icons
+// import SignUp from "layouts/authentication/sign-up";
+import EmployeeDetails from "layouts/employeeDetails";
+import Testimonials from "layouts/testimonials";
+import Career from "layouts/career";
+import ProjectAssigned from "layouts/projectAssigned";
+import Portfolio from "layouts/portfolio";
+import WorkIcon from "@mui/icons-material/Work";
+import FeedbackIcon from "@mui/icons-material/Feedback";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+import MiscellaneousServicesIcon from "@mui/icons-material/MiscellaneousServices";
 import Icon from "@mui/material/Icon";
+import Services from "layouts/services";
 
 const routes = [
   {
@@ -59,6 +68,14 @@ const routes = [
   },
   {
     type: "collapse",
+    name: "Employee Details",
+    key: "employee-details",
+    icon: <Icon fontSize="small">person</Icon>,
+    route: "/employeeDetails",
+    component: <EmployeeDetails />,
+  },
+  {
+    type: "collapse",
     name: "Tables",
     key: "tables",
     icon: <Icon fontSize="small">table_view</Icon>,
@@ -67,36 +84,81 @@ const routes = [
   },
   {
     type: "collapse",
-    name: "Billing",
-    key: "billing",
-    icon: <Icon fontSize="small">receipt_long</Icon>,
-    route: "/billing",
-    component: <Billing />,
+    name: "Services",
+    key: "services",
+    icon: <MiscellaneousServicesIcon fontSize="small" />,
+    route: "/services",
+    component: <Services />,
+    noCollapse: true,
   },
   {
     type: "collapse",
-    name: "RTL",
-    key: "rtl",
-    icon: <Icon fontSize="small">format_textdirection_r_to_l</Icon>,
-    route: "/rtl",
-    component: <RTL />,
+    name: "Testimonials",
+    key: "testimonials",
+    icon: <FeedbackIcon fontSize="small" />,
+    route: "/testimonials",
+    component: <Testimonials />,
+    noCollapse: true,
   },
   {
     type: "collapse",
-    name: "Notifications",
-    key: "notifications",
-    icon: <Icon fontSize="small">notifications</Icon>,
-    route: "/notifications",
-    component: <Notifications />,
+    name: "Career",
+    key: "career",
+    icon: <WorkIcon fontSize="small" />,
+    route: "/career",
+    component: <Career />,
+    noCollapse: true,
   },
   {
     type: "collapse",
-    name: "Profile",
-    key: "profile",
-    icon: <Icon fontSize="small">person</Icon>,
-    route: "/profile",
-    component: <Profile />,
+    name: "Projects-Assigned",
+    key: "projects-assigned",
+    icon: <AssignmentIcon fontSize="small" />,
+    route: "/projectAssigned",
+    component: <ProjectAssigned />,
+    noCollapse: true,
   },
+  {
+    type: "collapse",
+    name: "Portfolio",
+    key: "portfolio",
+    icon: <BusinessCenterIcon fontSize="small" />,
+    route: "/portfolio",
+    component: <Portfolio />,
+    noCollapse: true,
+  },
+  // {
+  //   type: "collapse",
+  //   name: "Billing",
+  //   key: "billing",
+  //   icon: <Icon fontSize="small">receipt_long</Icon>,
+  //   route: "/billing",
+  //   component: <Billing />,
+  // },
+  // {
+  //   type: "collapse",
+  //   name: "RTL",
+  //   key: "rtl",
+  //   icon: <Icon fontSize="small">format_textdirection_r_to_l</Icon>,
+  //   route: "/rtl",
+  //   component: <RTL />,
+  // },
+  // {
+  //   type: "collapse",
+  //   name: "Notifications",
+  //   key: "notifications",
+  //   icon: <Icon fontSize="small">notifications</Icon>,
+  //   route: "/notifications",
+  //   component: <Notifications />,
+  // },
+  // {
+  //   type: "collapse",
+  //   name: "Profile",
+  //   key: "profile",
+  //   icon: <Icon fontSize="small">person</Icon>,
+  //   route: "/profile",
+  //   component: <Profile />,
+  // },
   {
     type: "collapse",
     name: "Sign In",
@@ -105,14 +167,14 @@ const routes = [
     route: "/authentication/sign-in",
     component: <SignIn />,
   },
-  {
-    type: "collapse",
-    name: "Sign Up",
-    key: "sign-up",
-    icon: <Icon fontSize="small">assignment</Icon>,
-    route: "/authentication/sign-up",
-    component: <SignUp />,
-  },
+  // {
+  //   type: "collapse",
+  //   name: "Sign Up",
+  //   key: "sign-up",
+  //   icon: <Icon fontSize="small">assignment</Icon>,
+  //   route: "/authentication/sign-up",
+  //   component: <SignUp />,
+  // },
 ];
 
 export default routes;
